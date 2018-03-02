e649cfcd54e5459b1d293ed1890137042314583c
Mike Boers <github@mikeboers.com>
2018-03-02 17:30:04 -0500

    First pass at refactoring into a command usable for more than the first author.
    I've only barely tested the building aspect at this point. Beware!

---
+++ examples/zones/example.com.py
@@ -0,0 +1,20 @@
+
+z.A('@', '1.2.3.4')
+
+z.NS('@', 'ns1')
+z.NS('@', 'ns2')
+z.A('ns1', '1.2.3.4')
+z.A('ns2', '5.6.7.8')
+
+def google_apps(zone, name='@', **kwargs):
+    zone.comment('Google Apps - mail')
+    zone.MX(name, 1, 'ASPMX.L.GOOGLE.COM.')
+    zone.MX(name, 5, 'ALT1.ASPMX.L.GOOGLE.COM.')
+    zone.MX(name, 5, 'ALT2.ASPMX.L.GOOGLE.COM.')
+    zone.MX(name, 10, 'ASPMX2.GOOGLEMAIL.COM.')
+    zone.MX(name, 10, 'ASPMX3.GOOGLEMAIL.COM.')
+    spf = zone.spf(name)
+    spf.add('include', '_spf.google.com')
+    spf.default = spf.default or '-'
+
+google_apps(z)
--- zones/named.py
+++ zones/named.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import argparse
 import datetime
 import hashlib
@@ -10,35 +12,8 @@ import subprocess
 import sys
 import time
 
+from .zone import Zone
 
-here = os.path.dirname(__file__)
-os.chdir(here)
-sys.path.append('lib/zones')
-
-from zones import Zone
-import shared
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-b', '--build', action='store_true')
-parser.add_argument('--no-conf-check', action='store_true')
-parser.add_argument('-c', '--clean', action='store_true')
-parser.add_argument('-d', '--deploy', action='store_true')
-parser.add_argument('--no-reload', action='store_true')
-parser.add_argument('--no-live-check', action='store_true')
-args = parser.parse_args()
-
-
-now = datetime.datetime.utcnow()
-master_zone_names = []
-
-def create_zone(zonename):
-    master_zone_names.append(zonename)
-    return Zone(zonename,
-        primary_nameserver='ns1.mminternals.com.',
-        hostmaster_email='it+hostmaster@markmedia.co.',
-        ttl=300,
-    )
 
 def to_wire(name):
     parts = name.split('.')
@@ -46,114 +21,204 @@ def to_wire(name):
 assert hashlib.sha1(to_wire('domain.example.')).hexdigest() == '5960775ba382e7a4e09263fc06e7c00569b6a05c'
 
 
-# We use bind9's "catalog" feature for transferring zones to our
-# slave servers.
-catalog = create_zone('catalog.mminternals.com.')
-shared.mminternals_ns_v1(catalog)
-catalog.comment('Bind catalog')
-catalog.A('masters', '159.203.32.178') # This should be us.
-catalog.TXT('version', '1') # Required
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-c', '--clean', action='store_true')
+    parser.add_argument('-b', '--build', action='store_true')
+    parser.add_argument('-d', '--deploy', action='store_true')
+
+    parser.add_argument('-m', '--master')
+    parser.add_argument('-s', '--slave', action='append')
+    parser.add_argument('--catalog')
+
+    parser.add_argument('--primary-nameserver')
+    parser.add_argument('--hostmaster-email')
+    parser.add_argument('--default-ttl', type=int, default=300)
+
+    parser.add_argument('-z', '--zones-dir', default='zones')
+    parser.add_argument('--build-root', default='build')
+    parser.add_argument('--build-name', default=datetime.datetime.utcnow().strftime('%Y%m%d-%H%M%S'))
+
+    parser.add_argument('--no-conf-check', action='store_true')
+    parser.add_argument('--no-live-check', action='store_true')
+    parser.add_argument('--no-reload', action='store_true')
+
+    args = parser.parse_args()
 
 
-if args.build:
 
-    build_dir = os.path.join('build', now.strftime('%Y%m%d-%H%M%S'))
-    os.makedirs(build_dir)
+    if args.catalog:
+        print("--catalog is not supported yet.")
+        return 1
+        # We use bind9's "catalog" feature for transferring zones to our
+        # slave servers.
+        catalog = create_zone(args.catalog)
+        shared.mminternals_ns_v1(catalog)
+        catalog.comment('Bind catalog')
+        catalog.A('masters', args.master) # This should be us.
+        catalog.TXT('version', '1') # Required
 
-    for filename in os.listdir('zones'):
+    latest_build_symlink = os.path.join(args.build_root, 'latest')
+    if args.build:
+        build_dir = build(args)
+        try:
+            os.unlink(latest_build_symlink)
+        except OSError:
+            pass
+        os.symlink(os.path.abspath(build_dir), latest_build_symlink)
+    else:
+        try:
+            build_dir = os.readlink(latest_build_symlink)
+        except OSError:
+            print("No build dir exists; cannot continue.")
+            return 2
+
+    if args.clean:
+        clean(args, build_dir)
+
+    if not args.no_conf_check:
+        print('Checking zones and config...')
+        check_conf(args)
+
+    if args.deploy:
+
+        print('Linking to build/live...')
+        try:
+            os.unlink('build/live')
+        except OSError:
+            pass
+        os.symlink(os.path.abspath(build_dir), 'build/live')
+
+        if not args.no_conf_check:
+            print('Checking again...')
+            check_conf()
+
+        if not args.no_reload:
+            print('Reloading service...')
+            subprocess.check_call(['sudo', 'systemctl', 'reload', 'bind9'])
+
+        if not args.no_live_check:
+            print('Checking live local records...')
+            check_live()
+            for host in args.slave:
+                print('Checking live remote ({}) records...'.format(host))
+                check_live(host, delay=0.25, tries=10)
+
+
+
+def build(args):
+
+    build_dir = os.path.abspath(os.path.join(args.build_root, args.build_name))
+    build_zones_dir = os.path.join(build_dir, 'zones')
+    os.makedirs(build_zones_dir)
+
+    master_zone_names = []
+
+    def create_zone(zonename):
+        master_zone_names.append(zonename)
+        return Zone(zonename,
+            primary_nameserver=args.primary_nameserver,
+            hostmaster_email=args.hostmaster_email,
+            ttl=300,
+        )
+
+    for filename in os.listdir(args.zones_dir):
         if filename.startswith('.') or not filename.endswith('.py'):
             continue
         
         domain = os.path.splitext(filename)[0]
         zonename = domain + '.'
-        print domain
+        print(domain)
 
-
+        # TODO: Restore this.
         # Add it to the master catalog.
-        hash_ = hashlib.sha1(to_wire(zonename)).hexdigest()
-        catalog.PTR(hash_ + '.zones', zonename)
+        # hash_ = hashlib.sha1(to_wire(zonename)).hexdigest()
+        # catalog.PTR(hash_ + '.zones', zonename)
 
         # Create the zone and eval the script.
         zone = create_zone(zonename)
         namespace = {'zone': zone, 'z': zone}
-        execfile(os.path.join('zones', filename), namespace)
+        execfile(os.path.join(args.zones_dir, filename), namespace)
 
         # Add our check to it.
         zone.comment('Deployment check')
         zone.TXT('_serial', zone.serial_number, ttl=1)
 
-        path = os.path.join(build_dir, domain)
+        path = os.path.join(build_zones_dir, domain)
         with open(path, 'wb') as fh:
             fh.write(''.join(zone.iterdumps()))
 
+    # TODO: Restore the catalog.
     # Write out the catalog.
-    with open(os.path.join(build_dir, 'catalog.mminternals.com'), 'wb') as fh:
-        fh.write(''.join(catalog.iterdumps()))
+    # with open(os.path.join(build_dir, 'catalog.mminternals.com'), 'wb') as fh:
+        # fh.write(''.join(catalog.iterdumps()))
     
     # Write out the conf.
     with open(os.path.join(build_dir, 'named.conf.zones'), 'wb') as fh:
         for name in master_zone_names:
+            # TODO: Signal this another way.
             if name.startswith('rpz'):
                 extra = 'allow-query { none; };'
             else:
                 extra = ''
 
-            fh.write('''zone "%(zonename)s" {
+            fh.write('''zone "{zonename}" {{
                 type master;
-                file "/etc/bind/build/live/%(filename)s";
-                %(extra)s
-            };''' % dict(zonename=name, filename=name.strip('.'), extra=extra))
+                file "{build_dir}/zones/{filename}";
+                {extra}
+            }};'''.format(
+                zonename=name,
+                build_dir=build_dir,
+                filename=name.strip('.'),
+                extra=extra,
+            ))
             fh.write('\n\n') # An extra newline is required!
 
+    return build_dir
+
+
+def clean(args, build_dir):
+
+    print('Cleaning old builds...')
+
+    dont_clean = set(('latest', os.path.basename(build_dir)))
+    for name in os.listdir(args.build_root):
+        if name in dont_clean:
+            continue
+
+        print('   ', name)
+        path = os.path.join(args.build_root, name)
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            os.unlink(path)
+
+
+def check_conf(args):
     try:
-        os.unlink('build/latest')
-    except OSError:
-        pass
-    os.symlink(os.path.abspath(build_dir), 'build/latest')
 
-else:
+        subprocess.check_call(['named-checkconf', os.path.join(args.build_root, 'named.conf')])
 
-    build_dir = os.readlink('build/latest')
+        zone_dir = os.path.join(args.build_root, 'zones')
+        for name in os.listdir(zone_dir):
+            subprocess.check_call(['named-checkzone', name + '.', os.path.join(zone_dir, name)])
 
+        # The full thing as it stands.
+        subprocess.check_call(['named-checkconf'])
 
-if args.clean:
-    print 'Cleaning old builds...'
-    dont_clean = set(('live', 'latest', os.path.basename(build_dir)))
-    try:
-        dont_clean.add(os.path.basename(os.readlink('build/live')))
-    except:
-        pass
-    for name in os.listdir('build'):
-        if name not in dont_clean:
-            print '   ', name
-            path = os.path.join('build', name)
-            if os.path.isdir(path):
-                shutil.rmtree(path)
-            else:
-                os.unlink(path)
-
-
-def check_conf(live=False):
-    try:
-        for name in os.listdir(build_dir):
-            if name.startswith('named.conf'):
-                if live:
-                    subprocess.check_call(['named-checkconf', os.path.join(build_dir, name)])
-                else:
-                    continue
-            zonepath = os.path.join(build_dir, name)
-            subprocess.check_call(['named-checkzone', name + '.', zonepath])
-        subprocess.check_call(['named-checkconf', 'named.conf'])
-    except:
-        print 'Failed check!'
+    except subprocess.CalledProcessError:
+        print('Failed check!')
         exit(1)
+
 
 def check_live(server='localhost', delay=0.1, tries=3):
 
-    
     did_error = False
     for name in os.listdir('build/live'):
 
+        # TODO: Signal this another way.
         if name.startswith('rpz'):
             continue
 
@@ -162,7 +227,7 @@ def check_live(server='localhost', delay=0.1, tries=3):
         if not m:
             continue
         serial = m.group(1)
-        print ('    %s %s ' % (name, serial)), 
+        print('    %s %s ' % (name, serial), endl='')
         
         # Try twice. Sometimes it takes the kick from the first request
         # to clear the caches.
@@ -170,7 +235,7 @@ def check_live(server='localhost', delay=0.1, tries=3):
             output = subprocess.check_output(['dig', '@' + server, '+short', '_serial.' + name.strip('.'), 'TXT'])
             output = output.strip().strip('"')
             if serial == output:
-                print ' OK'
+                print(' OK')
                 break
             
             # We need to give it a fraction of a second.
@@ -179,43 +244,21 @@ def check_live(server='localhost', delay=0.1, tries=3):
             time.sleep(delay)
         
         else:
-            print '    ERROR: Got %s' % output
+            print('    ERROR: Got %s' % output)
             did_error = True
 
     if did_error:
-        print
-        print 'THE RUNTIME CHECKS DID NOT PASS!!!'
-        print
+        print()
+        print('THE RUNTIME CHECKS DID NOT PASS!!!')
+        print()
         exit(4)
 
-        print 'Everything checks out!'
+    print('Everything checks out!')
 
 
 
-if not args.no_conf_check:
-    print 'Checking zones and config...'
-    check_conf()
 
 
-if args.deploy:
-    print 'Linking to build/live...'
-    try:
-        os.unlink('build/live')
-    except OSError:
-        pass
-    os.symlink(os.path.abspath(build_dir), 'build/live')
-
-    if not args.no_conf_check:
-        print 'Checking again...'
-        check_conf()
-
-    if not args.no_reload:
-        print 'Reloading service...'
-        subprocess.check_call(['sudo', 'systemctl', 'reload', 'bind9'])
-
-    if not args.no_live_check:
-        print 'Checking local records...'
-        check_live()
-        print 'Checking remote (oxford) records...'
-        check_live('23.239.9.70', delay=0.25, tries=10)
+if __name__ == '__main__':
+    exit(main() or 0)
 
