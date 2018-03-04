d5e670df3205be8422058fbe589646c7985d9807
Mike Boers <github@mikeboers.com>
2018-03-04 11:57:07 -0500

    Tabs to spaces.

--- zones/record.py
+++ zones/record.py
@@ -1,19 +1,19 @@
 
 class Record(object):
 
-	def __init__(self, name, *data, **kwargs):
-		self.name = name
-		self.data = data
-		self.ttl = kwargs.pop('ttl', None)
-		self.class_ = kwargs.pop('class_', 'IN')
-		self.type = kwargs.pop('type', None)
+    def __init__(self, name, *data, **kwargs):
+        self.name = name
+        self.data = data
+        self.ttl = kwargs.pop('ttl', None)
+        self.class_ = kwargs.pop('class_', 'IN')
+        self.type = kwargs.pop('type', None)
 
-	def __str__(self):
-		return self.dumps().rstrip()
+    def __str__(self):
+        return self.dumps().rstrip()
 
-	def dumps(self, data=None):
-		parts = [self.name, self.ttl, self.class_, self.type or 'XXX']
-		parts.extend(data if data is not None else self.data)
-		parts = [str(x) for x in parts if x is not None]
-		parts = [('"%s"' % x if ' ' in x else x) for x in parts]
-		return ' '.join(parts) + '\n'
+    def dumps(self, data=None):
+        parts = [self.name, self.ttl, self.class_, self.type or 'XXX']
+        parts.extend(data if data is not None else self.data)
+        parts = [str(x) for x in parts if x is not None]
+        parts = [('"%s"' % x if ' ' in x else x) for x in parts]
+        return ' '.join(parts) + '\n'
--- zones/spf.py
+++ zones/spf.py
@@ -2,31 +2,31 @@ from .record import Record
 
 class SPF(Record):
 
-	def __init__(self, *args, **kwargs):
-		self.default = kwargs.pop('default', None)
-		kwargs['type'] = 'TXT'
-		super(SPF, self).__init__(*args, **kwargs)
-		self.data = list(self.data)
+    def __init__(self, *args, **kwargs):
+        self.default = kwargs.pop('default', None)
+        kwargs['type'] = 'TXT'
+        super(SPF, self).__init__(*args, **kwargs)
+        self.data = list(self.data)
 
-	def add(self, type, spec=None, action=''):
-		if type not in ('all', 'include', 'a', 'mx', 'ptr', 'ip4', 'ip6', 'exists'):
-			raise ValueError('Bad SPF type.')
-		if action not in ('', '+', '-', '?', '~'):
-			raise ValueError('Bad SPF action.')
-                if spec:
-		    self.data.append('%s%s:%s' % (action, type, spec))
-                else:
-                    self.data.append('%s%s' % (action, type))
+    def add(self, type, spec=None, action=''):
+        if type not in ('all', 'include', 'a', 'mx', 'ptr', 'ip4', 'ip6', 'exists'):
+            raise ValueError('Bad SPF type.')
+        if action not in ('', '+', '-', '?', '~'):
+            raise ValueError('Bad SPF action.')
+        if spec:
+            self.data.append('%s%s:%s' % (action, type, spec))
+        else:
+            self.data.append('%s%s' % (action, type))
 
-	def dumps(self):
+    def dumps(self):
 
-		if not (self.data or self.default):
-			return ''
+        if not (self.data or self.default):
+            return ''
 
-		parts = ['v=spf1']
-		parts.extend(self.data)
-		if self.default:
-			parts.append('%sall' % self.default)
+        parts = ['v=spf1']
+        parts.extend(self.data)
+        if self.default:
+            parts.append('%sall' % self.default)
 
-		return super(SPF, self).dumps(data=[' '.join(parts)])
+        return super(SPF, self).dumps(data=[' '.join(parts)])
 
