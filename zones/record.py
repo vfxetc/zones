1d95bdc03247804ff4b42f870fff5b9012a6627f
Mike Boers <mikeb@loftysky.com>
2018-06-19 10:32:16 -0400

    Add split_long for auto-splitting long records.

--- zones/record.py
+++ zones/record.py
@@ -8,6 +8,10 @@ class Record(object):
         self.class_ = kwargs.pop('class_', 'IN')
         self.type = kwargs.pop('type', None)
 
+        split = kwargs.pop('split_long', False)
+        if len(data) == 1 and split:
+            self.data = [data[0][i:i+255] for i in xrange(0, len(data[0]), 255)]
+
     def __str__(self):
         return self.dumps().rstrip()
 
